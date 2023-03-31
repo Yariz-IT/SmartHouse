13846a6690ef56385739635141e982c38d4ee8f1
Yariz-IT <116900067+Yariz-IT@users.noreply.github.com>
2023-03-31 08:32:53 +0700

    Delete selectRoomOnClick.js

--- selectRoomOnClick.js
+++
@@ -1,9 +0,0 @@
-// Выбор комнаты по клику
-
-DOM.rooms.querySelectorAll('.room').forEach(room => {
-    room.onclick = (event) => {
-        const chooseRoom = room.dataset.room
-        selectRoom(chooseRoom)
-    }
-})
-
