2e647162f3e5e8189d4c29b8d9371e87e671f0d7
Yariz-IT <116900067+Yariz-IT@users.noreply.github.com>
2023-03-31 08:32:01 +0700

    Delete setTemperatureKnob.js

--- setTemperatureKnob.js
+++
@@ -1,9 +0,0 @@
-// Установка значения кнопки включения температуры
-
-function setTemperatureKnob() {
-    if (roomsData[activeRoom].temperatureOff) {
-        DOM.temperatureKnob.classList.add('off')
-    } else {
-        DOM.temperatureKnob.classList.remove('off')
-    }
-}
