dcb6474980a32b2ad4ddd6cb2348c4389a123e7b
Yariz-IT <116900067+Yariz-IT@users.noreply.github.com>
2023-05-08 23:44:59 +0700

    Update  saveTemperature.js

--- js/ saveTemperature.js
+++ js/ saveTemperature.js
@@ -1,1 +1,8 @@
+// Сохранение температуры
+
+DOM.temperatureSaveButton.onclick = () => {
+    const temperature = +DOM.temperature.innerText
+    roomsData[activeRoom].temperature = temperature
+    alert('Temperature saved')
+}
 
